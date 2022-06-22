c8aa3ef8bfd1570b4f0d809799df9262437fc138
Anson Wang <ansor4@gmail.com>
2022-06-22 17:25:15 -0400

    Add faraday plumbing and Client class

--- client/client.rb
+++ client/client.rb
@@ -1,4 +1,14 @@
 require "faraday"
+require 'faraday/net_http'
+Faraday.default_adapter = :net_http
+
+class Client
+
+  # /artworks endpoint
+  url = "http://localhost:4567/artworks"
+  # Your retrieve function plus any additional functions go here ...
+
+end
 
 # /artworks endpoint
 url = "http://localhost:4567/artworks"
