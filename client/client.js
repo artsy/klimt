a92395cb6b8afea3e23d7f3a6a2b251da6d72523
Pavlos Vinieratos <pvinis@gmail.com>
2022-06-24 13:06:23 +0100

    second

--- client/client.js
+++ client/client.js
@@ -1,42 +1,62 @@
-const axios = require("axios");
+const axios = require('axios')
 
 // /artworks endpoint
-const artworks_url = "http://localhost:4567/artworks";
-const artist_url = "http://localhost:4567/artist";
+const artworks_url = 'http://localhost:4567/artworks'
+const artist_url = 'http://localhost:4567/artist'
+
+const isPrimary = (color) => ['yellow', 'red', 'blue'].includes(color)
 
 const formatForSaleData = (forSaleData) => {
-  
+	const { id, dominant_color, availability } = forSaleData
+	return {
+		id,
+		availability,
+		dominant_colors: dominant_color,
+		isPrimary: isPrimary(dominant_color),
+	}
 }
 
-const formatData = (data) => {
-  return {
-    soldPrimaryCount: 1,
-    nextPage: 2,
-    previousPage: null,
-    ids: data.map((item) => item.id),
-    for_sale: data.map((item) => item),
-  };
-};
+const formatData = (data, options) => {
+	// console.log(JSON.stringify(data, null, 2))
+	return {
+		previousPage: options.page - 1 === 0 ? null : options.page - 1,
+		nextPage: options.page + 1,
+		ids: data.map((item) => item.id),
+		for_sale: data
+			.filter((item) => item.availability === 'for_sale')
+			.map((item) => formatForSaleData(item)),
+		soldPrimaryCount: data.filter(
+			(item) => item.availability === 'sold' && isPrimary(item.dominant_color),
+		).length,
+	}
+}
+
+const PAGE_SIZE = 10
 
 // Your retrieve function plus any additional functions go here ...
-const retrieve = () => {
-  return new Promise((res, rej) => {
-    axios
-      .get(artworks_url, {
-        params: {
-          limit: 10,
-        },
-      })
-      .then(function (response) {
-        const formattedData = formatData(response.data);
-        res(formattedData);
-      })
-      .catch(function (error) {
-        rej(error);
-      });
-  });
-};
+const retrieve = (explicitOptions) => {
+	const options = { ...{ page: 1 }, ...explicitOptions }
+	const { page } = options
+
+	return new Promise((res, rej) => {
+		axios
+			.get(artworks_url, {
+				params: {
+					offset: (page - 1) * PAGE_SIZE,
+					limit: PAGE_SIZE,
+				},
+			})
+			.then(function (response) {
+				const formattedData = formatData(response.data, options)
+				// console.log(JSON.stringify(formattedData, null, 2))
+				res(formattedData)
+			})
+			.catch(function (error) {
+				rej(error)
+			})
+	})
+}
 
 module.exports = {
-  retrieve,
-};
+	retrieve,
+}
