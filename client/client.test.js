6aa6f3294d85ef6004dc65c6b0ef756863d5f514
Pavlos Vinieratos <pvinis@gmail.com>
2022-06-29 23:15:02 +0100

    spacing

--- client/client.test.js
+++ client/client.test.js
@@ -2,16 +2,17 @@ const axios = require('axios')
 const { retrieve } = require('./client')
 
 describe('basic', (done) => {
-	it('pings', () => {
-		axios
-			.get('/ping')
-			.then(function (output) {
-				expect(output).toEqual('pong')
-			})
-			.then(done)
-	})
+  it('pings', () => {
+    axios
+      .get('/ping')
+      .then(function (output) {
+        expect(output).toEqual('pong')
+      })
+      .then(done)
+  })
 })
 
+describe('client', (done) => {
   it('should return unfiltered results', (done) => {
     var expected = {"previousPage":null,"nextPage":2,"ids":[1,2,3,4,5,6,7,8,9,10],"for_sale":[{"id":2,"dominant_colors":"yellow","availability":"for_sale","isPrimary":true},{"id":4,"dominant_colors":"brown","availability":"for_sale","isPrimary":false},{"id":6,"dominant_colors":"blue","availability":"for_sale","isPrimary":true},{"id":8,"dominant_colors":"green","availability":"for_sale","isPrimary":false},{"id":10,"dominant_colors":"red","availability":"for_sale","isPrimary":true}],"soldPrimaryCount":1};
     retrieve().then(function(output){
